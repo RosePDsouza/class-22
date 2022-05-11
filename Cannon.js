dfd40812ca3dbf912c1df1b4aaf3b786ebe3ce54
RosePDsouza <102138619+RosePDsouza@users.noreply.github.com>
2022-05-11 15:47:48 +0530

    Add files via upload

---
+++ Cannon.js
@@ -0,0 +1,21 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonimg = loadImage("assets/canon.png" )
+    this.cannonBase = loadImage("assets/cannonBase.png")
+  }
+  display(){
+    push()
+    imageMode(CENTER)
+    image (  this.cannonimg,this.x,this.y,this.width,this.height);
+
+    pop()
+    image( this.cannonBase,70,20,200,200)
+    noFill()
+  }
+  
+}
